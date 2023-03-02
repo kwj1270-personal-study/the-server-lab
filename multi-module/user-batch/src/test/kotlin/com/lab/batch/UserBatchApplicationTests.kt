a5541b2e169da24fcefb511b3c895a9a75422c6b
kwj1270 <kwj1270@naver.com>
2023-03-03 05:17:17 +0900

    feat(kwj1270): init kotlin-spring-multi-module project2

---
+++ multi-module/user-batch/src/test/kotlin/com/lab/batch/UserBatchApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.lab.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
