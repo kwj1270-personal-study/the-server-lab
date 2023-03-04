c19d7204e81f5e102c63fe1bac2b97e5d9b8fd50
kwj1270 <kwj1270@naver.com>
2023-03-04 21:56:03 +0900

    feat(kwj1270): init kotlin-dgs

---
+++ dgs/src/main/kotlin/com/lab/dgs/Show.kt
@@ -0,0 +1,3 @@
+package com.lab.dgs
+
+data class Show(val title: String, val releaseYear: Int)
---
+++ dgs/src/test/kotlin/com/lab/dgs/DgsApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.lab.dgs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DgsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
