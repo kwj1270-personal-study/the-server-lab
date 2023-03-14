7faca15d0a13d3d9379deab4aa99684cfc49cc66
kwj1270 <kwj1270@naver.com>
2023-03-14 20:14:43 +0900

    refactor(kwj1270): refactor kotlin-query-offloading logic

--- kotlin-query-offloading/src/main/kotlin/com/lab/queryoffloading/config/datasource/RoutingDatasourceConfiguration.kt
+++ kotlin-query-offloading/src/main/kotlin/com/lab/queryoffloading/config/datasource/RoutingDatasourceConfiguration.kt
@@ -1,6 +1,10 @@
 package com.lab.queryoffloading.config.datasource
 
+import com.zaxxer.hikari.HikariDataSource
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -9,22 +13,37 @@ import javax.sql.DataSource
 
 @Configuration
 class RoutingDatasourceConfiguration(
-    private val customRoutingDataSources: List<CustomRoutingDataSource>
 ) {
 
-    @Bean
+    @Bean(name = ["masterDataSource"])
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    fun masterDataSource(): DataSource = DataSourceBuilder.create()
+        .type(HikariDataSource::class.java)
+        .build()
+
+    @Bean(name = ["slaveDataSource"])
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    fun slaveDataSource(): DataSource = DataSourceBuilder.create()
+        .type(HikariDataSource::class.java)
+        .build()
+        .apply {
+            isReadOnly = true
+        }
+
     @Primary
-    fun datasource(@Qualifier("customRoutingDatasource") dataSource: DataSource): DataSource {
-        return LazyConnectionDataSourceProxy(dataSource)
+    @ConditionalOnBean(name = ["datasourceRouter"])
+    @Bean(name = ["datasource"])
+    fun datasource(@Qualifier("datasourceRouter") datasourceRouter: DataSource): DataSource {
+        return LazyConnectionDataSourceProxy(datasourceRouter)
     }
 
-    @Bean
-    fun customRoutingDatasource(): DataSource {
-        val routingDataSourceMap =
-            customRoutingDataSources.associate { RoutingDataSourceRole.of(it.isReadOnly()) to it.toRouterDatasource() }
-        return DataSourceRouter(
-            routingDataSourceMap,
-            routingDataSourceMap[RoutingDataSourceRole.READ_WRITE]!!
-        )
+    @Bean(name = ["datasourceRouter"])
+    @ConditionalOnBean(name = ["masterDataSource", "slaveDataSource"])
+    fun datasourceRouter(
+        @Qualifier("slaveDataSource") slaveDataSource: DataSource,
+        @Qualifier("masterDataSource") masterDataSource: DataSource,
+    ): DataSource {
+        val dataSourcesMap: Map<String, DataSource> = mapOf("master" to masterDataSource, "slave" to slaveDataSource)
+        return DataSourceRouter(dataSourcesMap, masterDataSource)
     }
 }
